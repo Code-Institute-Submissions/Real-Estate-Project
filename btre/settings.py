d394c6169a5ef89e14220877e2d9cde33a3c14af
Yari-Carelli <yaricarelli@hotmail.it>
2022-09-23 12:53:32 +0000

    Pages template and base layout

--- btre/settings.py
+++ btre/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'btre.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE-DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
