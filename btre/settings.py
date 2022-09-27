5c4ee564e76ceba0e1a4fc54d8cd801fb8059667
Yari-Carelli <yaricarelli@hotmail.it>
2022-09-27 16:57:36 +0000

    Set debug to false

--- btre/settings.py
+++ btre/settings.py
@@ -27,9 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-debug = os.getenv('DEBUG', 'FALSE') is 'TRUE'
+DEBUG = False
 
 ALLOWED_HOSTS = ['real-estate-project-yc.herokuapp.com', 'localhost']
 
